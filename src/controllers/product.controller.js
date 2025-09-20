60949ef962bf1509880bb5522c7da9a1da17a34e
trishalmandrik <trishal.mandrik@springernature.com>
2025-09-20 23:37:57 +0530

    add listAllProducts get endpoint

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,7 +3,7 @@ const db = require('../config/database')
 exports.createProduct = async (req, res) => {
     const { product_name, quantity, price } = req.body;
     const { rows } = await db.query(
-        "Insert into products (productName, quantity, price) values ($1, $2, $3)",
+        "Insert into public.products (productName, quantity, price) values ($1, $2, $3)",
         [product_name, quantity, price]
     );
 
@@ -13,4 +13,9 @@ exports.createProduct = async (req, res) => {
             product: { product_name, quantity, price }
         }
     })
+}
+
+exports.listAllProducts = async (req, res) => {
+    const response = await db.query('Select * from public.products order by productname');
+    res.status(200).send(response.rows);
 }
