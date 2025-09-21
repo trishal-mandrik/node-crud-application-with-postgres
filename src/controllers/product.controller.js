4ab8e5d8f48d713f29fbd49d50f113e422280601
trishalmandrik <trishal.mandrik@springernature.com>
2025-09-21 22:01:59 +0530

    add updateProductById and deleteProductById route

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,8 +1,8 @@
 const db = require('../config/database')
 
 exports.createProduct = async (req, res) => {
-    const { product_name, quantity, price } = req.body;
-    const { rows } = await db.query(
+    const {product_name, quantity, price} = req.body;
+    await db.query(
         "Insert into public.products (productName, quantity, price) values ($1, $2, $3)",
         [product_name, quantity, price]
     );
@@ -10,7 +10,7 @@ exports.createProduct = async (req, res) => {
     res.status(201).send({
         message: "Product added successfully",
         body: {
-            product: { product_name, quantity, price }
+            product: {product_name, quantity, price}
         }
     })
 }
@@ -24,4 +24,17 @@ exports.findProductById = async (req, res) => {
     const productId = parseInt(req.params.id);
     const response = await db.query('select * from public.products where productid = $1', [productId])
     res.status(200).send(response.rows);
+}
+
+exports.updateProductById = async (req, res) => {
+    const productId = parseInt(req.params.id);
+    const {product_name, quantity, price} = req.body;
+    await db.query('update public.products set productname = $1, quantity = $2, price = $3 where productid = $4', [product_name, quantity, price, productId]);
+    res.status(200).send({message: 'Product updated successfully'});
+}
+
+exports.deleteProductById = async (req, res) => {
+    const productId = parseInt(req.params.id);
+    await db.query('delete from public.products where productid = $1', [productId]);
+    res.status(200).send({message: 'Product deleted successfully'});
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,5 +4,7 @@ const productController = require('../controllers/product.controller')
 router.post('/products', productController.createProduct);
 router.get('/products', productController.listAllProducts);
 router.get('/products/:id', productController.findProductById);
+router.put('/products/:id', productController.updateProductById);
+router.delete('/products/:id', productController.deleteProductById);
 
 module.exports = router;
